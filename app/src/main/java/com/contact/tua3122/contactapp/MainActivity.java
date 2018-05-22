d17a0e0ec507181c3158d045d2c12fbe3cf5ef24
Allen Tu <allentu3@gmail.com>
2018-05-22 08:10:18 -0700

    ContactApp

---
+++ app/src/main/java/com/contact/tua3122/contactapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.contact.tua3122.contactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
